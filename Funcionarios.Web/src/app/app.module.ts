87269be00cd15ecc5ef16d0b7f886d4ec8f9bd51
Rafa Lohmann <raflohmann@gmail.com>
2018-04-14 08:29:37 -0300

    Revert "Flat Able Angular Lite Free Angular 4 Admin Template"
    This reverts commit 54aa6eb22e4660a41341ab319f44af7687df11ce.

--- Funcionarios.Web/src/app/app.module.ts
+++ Funcionarios.Web/src/app/app.module.ts
@@ -1,40 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { SharedModule } from './shared/shared.module';
 
-import { AppRoutes } from './app.routing';
+
 import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-import { BreadcrumbsComponent } from './layouts/main-layout/breadcrumbs/breadcrumbs.component';
-import { TitleComponent } from './layouts/main-layout/title/title.component';
-
-import {LocationStrategy, PathLocationStrategy} from '@angular/common';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainLayoutComponent,
-    AuthLayoutComponent,
-    BreadcrumbsComponent,
-    TitleComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    SharedModule,
-    RouterModule.forRoot(AppRoutes),
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
-  providers: [
-    { provide: LocationStrategy, useClass: PathLocationStrategy }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
